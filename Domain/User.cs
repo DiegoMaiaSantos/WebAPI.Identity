b5877625fe9994fbf3e2b4bb65029ca6f025e031
Diego Santos <diegom.santos03@gmail.com>
2025-01-21 20:41:51 -0300

    chore: criado a camada domain da WebAPI e desenvolvendo suas classes e regras.

---
+++ Domain/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain
+{
+    public class User : IdentityUser
+    {
+        public string? NomeCompleto { get; set; }
+        public string? Member { get; set; } = "Member";
+        public string OrgId { get; set; }
+        public List<UserRole> UserRoles { get; set; }
+    }
+}
