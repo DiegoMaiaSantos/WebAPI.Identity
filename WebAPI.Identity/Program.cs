6933ae3994fb4df7ea2bc6d1d1b61c7a360f3438
Diego Santos <diegom.santos03@gmail.com>
2025-01-23 18:34:01 -0300

    chore: connection ao dbo funcionando, configuracoes na program relacionadas ao DbContext e Identity realizadas.

--- WebAPI.Identity/Program.cs
+++ WebAPI.Identity/Program.cs
@@ -1,3 +1,5 @@
+using Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Repository;
 
@@ -5,7 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add context to the dbo
 builder.Services.AddDbContext<WebAPIDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAPIConnextion")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAPIConnection"), b =>
+        b.MigrationsAssembly("WebAPI.Identity")));
+
+builder.Services.AddIdentity<User, Role>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = true;
+
+    options.Lockout.MaxFailedAccessAttempts = 3;
+    options.Lockout.AllowedForNewUsers = true;
+})
+.AddEntityFrameworkStores<WebAPIDbContext>()
+.AddDefaultTokenProviders();
 
 // Add services to the container.
 
