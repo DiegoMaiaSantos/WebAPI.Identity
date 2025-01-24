012127185f1afb6d4561c5e08c21c1a5c70abb36
Diego Santos <diegom.santos03@gmail.com>
2025-01-24 18:32:47 -0300

    chore: created the Register method and created the private method GenerateJWToken in UsersController.

--- WebAPI.Identity/Controllers/UsersController.cs
+++ WebAPI.Identity/Controllers/UsersController.cs
@@ -2,6 +2,12 @@
 using Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebAPI.Identity.Dto;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,7 +22,7 @@ namespace WebAPI.Identity.Controllers
         private readonly SignInManager<User> _signInManager;
         private readonly IMapper _mapper;
 
-        public UsersController(IConfiguration configuration, UserManager<User> userManager, 
+        public UsersController(IConfiguration configuration, UserManager<User> userManager,
             SignInManager<User> signInManager, IMapper mapper)
         {
             _configuration = configuration;
@@ -41,8 +47,87 @@ namespace WebAPI.Identity.Controllers
 
         // POST api/<UsersController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Register(UserDto dto)
         {
+            try
+            {
+                var user = await _userManager.FindByNameAsync(dto.UserName);
+
+                if (user == null)
+                {
+                    user = new User()
+                    {
+                        UserName = dto.UserName,
+                        Email = dto.UserName
+                    };
+
+                    var result = await _userManager.CreateAsync(user, dto.Password);
+
+                    if (result.Succeeded)
+                    {
+                        var appUser = await _userManager.Users.FirstOrDefaultAsync(u =>
+                            u.NormalizedUserName == dto.UserName.ToUpper());
+
+                        var token = GenerateJWToken(appUser).Result;
+
+                        var confirmationEmail = Url.Action("ConfirmEmailAddress", "Home",
+                            new
+                            {
+                                token = token,
+                                email = user.Email
+                            }, Request.Scheme);
+
+                        System.IO.File.WriteAllText("confirmationEmail.txt", confirmationEmail);
+                    }
+                }
+                return Unauthorized();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Error {ex.Message}");
+            }
+        }
+
+        private async Task<string> GenerateJWToken(User user)
+        {
+            try
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Name, user.UserName)
+                };
+
+                var roles = await _userManager.GetRolesAsync(user);
+
+                foreach (var role in roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+                var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+                    _configuration.GetSection("AppSettings:Token").Value));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+                var tokenDescription = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(claims),
+                    Expires = DateTime.Now.AddDays(1),
+                    SigningCredentials = creds
+                };
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                var token = tokenHandler.CreateToken(tokenDescription);
+
+                return tokenHandler.WriteToken(token);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         // PUT api/<UsersController>/5
